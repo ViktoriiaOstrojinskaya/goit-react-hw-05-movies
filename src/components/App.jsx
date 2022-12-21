7918132a054bce103614dc4433e2e507a70bdf7f
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-12-21 10:57:34 -0500

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template hw-5
+      React hw-5
     </div>
   );
 };
